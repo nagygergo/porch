7fd48b1a1142c45871b30c05c96532a43cacc391
Gergely Nagy <gergely.l.nagy@ericsson.com>
2024-03-18 13:07:19 +0100

    Move client/informer/lister/conversion gen to api pkg
    - Relative package references like ../../generated/apis appear in generated go file module imports, instead of github.com/nephio-project....
    - client gen cannot handle outputting to a file tree with package path awareness without

--- api/doc.go
+++ api/doc.go
@@ -13,5 +13,9 @@
 // limitations under the License.
 
 // +domain=kpt.dev
+//go:generate go run k8s.io/code-generator/cmd/client-gen@v0.29.2  --plural-exceptions PackageRevisionResources:PackageRevisionResources  --input-base "github.com/nephio-project/porch"  --input-dirs ""  --input "api/porch/v1alpha1"  --output-package "github.com/nephio-project/porch/api/generated/clientset"   --clientset-name versioned  --trim-path-prefix "github.com/nephio-project/porch/api"  --go-header-file ../scripts/boilerplate.go.txt
+//go:generate go run k8s.io/code-generator/cmd/lister-gen --input-dirs github.com/nephio-project/porch/api/porch/v1alpha1 -p ../../generated/listers --go-header-file ../../../scripts/boilerplate.go.txt
+//go:generate go run k8s.io/code-generator/cmd/informer-gen --input-dirs github.com/nephio-project/porch/api/porch/v1alpha1 --versioned-clientset-package github.com/nephio-project/porch/api/generated/clientset/versioned --listers-package github.com/nephio-project/porch/api/generated/listers --internal-clientset-package github.com/nephio-project/porch/api/generated/informers/internalversion -p github.com/nephio-project/porch/api/generated/informers --plural-exceptions PackageRevisionResources:PackageRevisionResources --go-header-file ../scripts/boilerplate.go.txt
+//go:generate go run k8s.io/code-generator/cmd/conversion-gen --input-dirs ./porch -O zz_generated.conversion --go-header-file ../scripts/boilerplate.go.txt
 
 package apis
